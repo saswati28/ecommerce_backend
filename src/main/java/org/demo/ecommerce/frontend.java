e351a8173e19a61d21109b37b987e3e7308fec2a
Saswati <Saswati@saswati-hp>
2021-01-07 22:15:22 +0530

    first commit

---
+++ src/main/java/org/demo/ecommerce/frontend.java
@@ -0,0 +1,10 @@
+package org.demo.ecommerce;
+
+public class frontend {
+	public static void main(String abc[]) {
+		System.out.println("hello,i am saswati");
+		//String abc[2]="abc";
+		
+		
+	}
+}

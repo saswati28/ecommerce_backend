40326e158c42ea7167d6cf491fa1b76fa04e700e
Saswati <Saswati@saswati-hp>
2021-01-07 22:36:43 +0530

    file name changed.

---
+++ src/main/java/org/demo/ecommerce/Backend.java
@@ -0,0 +1,13 @@
+package org.demo.ecommerce;
+
+public class Backend {
+	public static void main(String args[]) {
+		System.out.println("hello,i am saswati");
+		String abc[]= {"abc","cde","efg"};
+		for(int i=0;i<abc.length;i++)
+		{
+			System.out.println(abc[i]);
+		}
+		
+	}
+}
